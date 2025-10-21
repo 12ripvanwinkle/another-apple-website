b908f073eb27ab97d05b4f17c9ce449be100f904
Nitesh <niteshanthony3@gmail.com>
2025-10-20 21:29:50 -0500

    implemented the 3D macbook model with lightings

--- macbook_gsap_app/src/components/ProductViewer.jsx
+++ macbook_gsap_app/src/components/ProductViewer.jsx
@@ -3,6 +3,8 @@ import useMacbookStore from '../store'
 import clsx from 'clsx'
 import { Canvas } from '@react-three/fiber'
 import {Box, OrbitControls} from "@react-three/drei";
+import MacbookModel14 from './models/Macbook-14';
+import StudioLights from './StudioLights';
 
 const ProductViewer = () => {
   const {color, scale, setColor, setScale} = useMacbookStore()
@@ -35,8 +37,10 @@ const ProductViewer = () => {
             </div>
         </div>
 
-        <Canvas id="canvas camera={{ position: [0, 0, 5] }}, fov: 50, near: 0.1, far: 100}">
-            <Box position={[0, 0, 0]} scale={10 * scale} material-color = {color}></Box>
+        <Canvas id="canvas" camera={{ position: [0, 0, 5], fov: 50, near: 0.1, far: 100 }}>
+            <StudioLights />
+
+            <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
 
             <OrbitControls enableZoom={false} />
         </Canvas>
