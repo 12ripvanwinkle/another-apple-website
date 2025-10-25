17a7f714215ffd0109f72b8ff0df2bdd63c3fd8f
Nitesh <niteshanthony3@gmail.com>
2025-10-25 09:45:57 -0500

    implemented the 3d model rending functions

--- macbook_gsap_app/src/App.jsx
+++ macbook_gsap_app/src/App.jsx
@@ -1,6 +1,11 @@
-import Hero from "./components/Hero.jsx"
-import NavBar from "./components/NavBar.jsx"
-import ProductViewer from "./components/ProductViewer.jsx"
+import React from "react";
+import Hero from "./components/Hero.jsx";
+import NavBar from "./components/NavBar.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from "gsap";
+import {ScrollTrigger, SplitText} from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
--- macbook_gsap_app/src/components/ProductViewer.jsx
+++ macbook_gsap_app/src/components/ProductViewer.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import useMacbookStore from '../store'
 import clsx from 'clsx'
 import { Canvas } from '@react-three/fiber'
-import {Box, OrbitControls} from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import MacbookModel14 from './models/Macbook-14';
-import StudioLights from './StudioLights';
+import { useMediaQuery } from 'react-responsive';
+
+
+import StudioLights from './three/StudioLights.jsx';
+import ModelSwitcher from './three/ModelSwitcher.jsx';
 
 const ProductViewer = () => {
   const {color, scale, setColor, setScale} = useMacbookStore()
+
+  const isMobile = useMediaQuery({ query: '(max-width: 1024px)'});
 
   return (
     <section id="product-viewer">
@@ -15,21 +21,21 @@ const ProductViewer = () => {
         <h2>Take a closer look</h2>
 
         <div className="controls">
-            <p className="info">MacbookPro {scale} in {color}</p>
+            <p className="info">MacbookPro {scale === 0.06 ? '14"' : '16"'} in {color === '#adb5bd' ? 'Silver' : 'Space Black'}</p>
 
             <div className='flex-center gap-5 mt-5'>
                 <div className="color-control">
-                    <div onClick={() => setColor('#adb5bd')} className= {clsx ('bg-neutral-300', color === '#adb5bd' && 'active')} />
+                    <div onClick={() => setColor('#adb5bd')} className={clsx('bg-neutral-300', color === '#adb5bd' && 'active')} />
 
-                    <div onClick={() => setColor('#2e2c2e')} className= {clsx ('bg-neutral-900', color === '#2e2c2e' && 'active')} />
+                    <div onClick={() => setColor('#2e2c2e')} className={clsx('bg-neutral-900', color === '#2e2c2e' && 'active')} />
                 </div>
 
                 <div className="size-control">
-                    <div onClick={() => setScale(0.06)} className= {clsx (scale === 0.06 ? 'bg-white text-black' : 'bg-transparent text-white')} >
+                    <div onClick={() => setScale(0.06)} className={clsx(scale === 0.06 ? 'bg-white text-black' : 'bg-transparent text-white')} >
                         <p>14"</p>
                     </div>
 
-                    <div onClick={() => setScale(0.08)} className= {clsx (scale === 0.08 ? 'bg-white text-black' : 'bg-transparent text-white')} >
+                    <div onClick={() => setScale(0.08)} className={clsx(scale === 0.08 ? 'bg-white text-black' : 'bg-transparent text-white')} >
                         <p>16"</p>
                     </div>
 
@@ -40,12 +46,8 @@ const ProductViewer = () => {
         <Canvas id="canvas" camera={{ position: [0, 0, 5], fov: 50, near: 0.1, far: 100 }}>
             <StudioLights />
 
-            <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
-
-            <OrbitControls enableZoom={false} />
+            <ModelSwitcher scale={isMobile ? scale - 0.03: scale} isMobile={isMobile}/>
         </Canvas>
-
-        <p className="text-white text-4xl">Render Canvas</p>
     </section>
   )
 }
